d8a3f9e82c064bdff17755b7b9fe1983614ada93
adber94 <adber1503@gmail.com>
2020-07-02 18:09:30 -0500

    Update index.php

--- index.php
+++ index.php
@@ -143,7 +143,7 @@ $appName = explode('.', $domain)[0];
                     </p>
                     <pre class="pre-item">
 "title" => "Item De Teste - Por Favor, Não Ofertar! --kc:off",
-        "category_id" => "MLM1912",
+        "category_id" => "MLM1953",
         "price" => 10,
         "currency_id" => "MXN",
         "available_quantity" => 1,
@@ -198,7 +198,7 @@ $appName = explode('.', $domain)[0];
                         // We construct the item to POST
                         $item = array(
                             "title" => "Item De Teste - Por Favor, Não Ofertar! --kc:off",
-        "category_id" => "MLM1912",
+        "category_id" => "MLM1953",
         "price" => 10,
         "currency_id" => "MXN",
         "available_quantity" => 1,
